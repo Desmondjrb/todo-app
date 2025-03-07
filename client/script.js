fe26bab28754405e1040a6d17445deaafdcb8365
Desmondjrb <desmondjrb@nycstudents.net>
2025-03-07 14:50:22 +0000

    changes

--- client/script.js
+++ client/script.js
@@ -6,17 +6,35 @@ let list = document.getElementById('todoList')
 // Fetch all todos when page loads
 async function fetchTodos() {
   // add your code
-  fetch("/api/todos")
+  try{
+    const response = await  fetch("/api/todos", {
+      method: "POST"
+    })
+    if(!response.ok){
+       throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    console.log(json)
+  } catch(error){
+    console.error(error.message);
+  }
+ 
 }
 
 // Display todos in the list
 function displayTodos(todos) {
   // add your code
+ todos = fetchTodos()
+
+  list.append(todos)
+  console.log(list)
 }
 
 // Handle form submission
 todoForm.addEventListener("submit", async (e) => {
   // add your code
+  e.preventDefault();
+  displayTodos()
 });
 
 // Load todos when page loads
